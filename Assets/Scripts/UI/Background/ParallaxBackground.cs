12f69d42442b5ec8ec5bfdddd840e43eabf86871
PhiLong1st <philongnguyen1704@gmail.com>
2026-03-07 15:48:59 +0700

    feat: add parallax background

---
+++ Assets/Scripts/UI/Background/ParallaxBackground.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParallaxBackground : MonoBehaviour
+{
+  [SerializeField] private float moveSpeed = 0.5f;
+  private SpriteRenderer _backgroundImage;
+  private float _backgroundWidth;
+
+  private void Awake()
+  {
+    _backgroundImage = GetComponent<SpriteRenderer>();
+    _backgroundWidth = _backgroundImage.bounds.size.x;
+  }
+
+  private void Update()
+  {
+    float moveX = moveSpeed * Time.deltaTime;
+    transform.position += new Vector3(moveX, 0);
+
+    if (Mathf.Abs(transform.position.x) - _backgroundWidth > 0)
+    {
+      transform.position = new Vector3(0, transform.position.y);
+    }
+  }
+}
