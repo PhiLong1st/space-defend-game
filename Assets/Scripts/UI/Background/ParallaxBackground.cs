cd5c470841cc9e9b2a1cc4a8da642843d213c0d4
PhiLong1st <philongnguyen1704@gmail.com>
2026-03-10 15:35:37 +0700

    fix: Fix Parallax background logic

--- Assets/Scripts/UI/Background/ParallaxBackground.cs
+++ Assets/Scripts/UI/Background/ParallaxBackground.cs
@@ -4,13 +4,13 @@ using UnityEngine.UI;
 public class ParallaxBackground : MonoBehaviour
 {
   [SerializeField] private float moveSpeed = 0.5f;
-  private SpriteRenderer _backgroundImage;
-  private float _backgroundWidth;
+  private SpriteRenderer _image;
+  private float _imageWidth;
 
   private void Awake()
   {
-    _backgroundImage = GetComponent<SpriteRenderer>();
-    _backgroundWidth = _backgroundImage.bounds.size.x;
+    _image = GetComponent<SpriteRenderer>();
+    _imageWidth = _image.sprite.texture.width / _image.sprite.pixelsPerUnit;
   }
 
   private void Update()
@@ -18,7 +18,7 @@ public class ParallaxBackground : MonoBehaviour
     float moveX = moveSpeed * SpaceshipController.Instance.CurrentMovementSpeed * Time.deltaTime;
     transform.position += new Vector3(moveX, 0);
 
-    if (Mathf.Abs(transform.position.x) - _backgroundWidth > 0)
+    if (Mathf.Abs(transform.position.x) - _imageWidth > 0)
     {
       transform.position = new Vector3(0, transform.position.y);
     }
